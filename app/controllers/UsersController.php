445f9082e502b1f8c4207467049b81aff170243f
EmulBopper <elamanovb@gmail.com>
2016-04-13 18:34:23 +0500

    register page

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -3,7 +3,11 @@
 class UsersController extends ControllerBase
 {
     public function indexAction() {
-        $user = Users::findfirst(1);
-        $this->view->setVar('user', $user);
+
+    }
+
+    public function create() {
+        $user = new User;
+
     }
 }
