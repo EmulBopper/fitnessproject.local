732f777267b7823b90c68086c9e4fa0513fb67ea
EmulBopper <elamanovb@gmail.com>
2016-04-19 22:05:35 +0500

    25

--- app/controllers/UsersController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class UsersController extends ControllerBase
-{
-    public function indexAction() {
-        $user = Users::findfirst(1);
-        $this->view->setVar('user', $user);
-    }
-}
